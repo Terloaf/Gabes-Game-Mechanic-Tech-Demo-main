0fa37af2762154f583369b9820923e1f4ff481d0
Terloaf <greenyryan2501@gmail.com>
2026-03-13 16:31:42 -0300

    Platforms now able to move between multiple points

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -1,29 +1,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Rendering;
 
+
+[RequireComponent(typeof(Rigidbody))]
 public class MovingPlatform : MonoBehaviour
 {
     //private Vector3 _startPos;
 
     //public Vector3 _targetPos;
-
     [SerializeField]
     List<Vector3> _points;
 
-    public int _speed;
+    public float _speed;
 
-    private Rigidbody _rb;
+    Rigidbody _rb;
     private void Start()
     {
-        
-    }
+        _rb = GetComponent<Rigidbody>();
+        _rb.isKinematic = true;
+
+        if (_points.Count == 0) return;
+
+        transform.position = _points[0];
+
+        if(_points.Count > 1)
+        {
+            StartCoroutine(MovePlatform());
+        }
 
 
-    private void Update()
-    {
-        StartCoroutine(MovePlatform());
     }
+
 
     IEnumerator MovePlatform()
     {
@@ -31,11 +40,13 @@ public class MovingPlatform : MonoBehaviour
         {
             while (Vector3.Distance(_rb.position, _points[i]) > 0.1)
             {
-                _rb.MovePosition(Vector3.MoveTowards(_rb.position, _points[i], _speed * Time.deltaTime));
+                _rb.MovePosition(Vector3.MoveTowards(_rb.position, _points[i], _speed * Time.fixedDeltaTime));
                 yield return null;
             }
 
             yield return new WaitForSeconds(1);
+
+            if(i == _points.Count - 1)
             {
                 i = 0;
 
