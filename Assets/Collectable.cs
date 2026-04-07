03f800f19718a863dc2c071b49ed8bc4c0930bb0
Terloaf <greenyryan2501@gmail.com>
2026-04-07 11:53:51 -0300

    Added Collectables and score

---
+++ Assets/Collectable.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private int _score = 0;
+    public TMP_Text _text;
+
+
+    private void Update()
+    {
+        _text.text = "Score: " + _score.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            _score += 1;
+            Destroy(other.gameObject);
+            
+        }
+    }
+
+}
