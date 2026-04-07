125d7226795c7384f979d68e422ac7adc7a01d09
Terloaf <greenyryan2501@gmail.com>
2026-04-07 13:11:27 -0300

    Added Teleporters that changes the players position and has a cooldown indicated by change in colour

--- Assets/Teleporter.cs
+++ Assets/Teleporter.cs
@@ -1,16 +1,37 @@
+using System.Collections;
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject _Desination;
+    public Material _default;
+    private Teleporter _lastStepped;
+    private Vector3 _position;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+           
+            _lastStepped = this;
+
+            _Desination.GetComponent<Collider>().enabled = false;
+            other.gameObject.transform.position = _Desination.transform.position;
+            StartCoroutine(Cooldown());
+
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Cooldown()
     {
-        
+        gameObject.GetComponent<Renderer>().material.color = new Color(1f, 0f, 0f);
+        _Desination.gameObject.GetComponent<Renderer>().material.color = new Color(1f, 0f, 0f);
+        yield return new WaitForSeconds(3);
+        gameObject.GetComponent<Renderer>().material.color = _default.color;
+        _Desination.gameObject.GetComponent<Renderer>().material.color = _default.color;
+        _Desination.GetComponent<Collider>().enabled = true;
     }
+
+
 }
+
